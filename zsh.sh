5cd9656137d5599169cd7b95efe0c1226e4d86b4
MRuihoo <mrh16@mails.tsinghua.edu.cn>
2019-06-19 15:53:15 +0800

    feat(#tmux): tmux install script

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,5 @@
 sudo apt update
-sudo apt upgrade
-sudo apt install zsh
+sudo apt -y upgrade
+sudo apt -y install zsh
 chsh -s /bin/zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-
