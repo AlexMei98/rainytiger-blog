378e3f6850f50ab9de3ebaf0914b41a8ec6622f5
MRuihoo <mrh16@mails.tsinghua.edu.cn>
2019-06-19 15:58:52 +0800

    feat(#conf): add config files script

---
+++ config.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/MRuihoo/rainytiger-blog/master/.tmux.conf
+wget https://raw.githubusercontent.com/MRuihoo/rainytiger-blog/master/.vimrc
+wget https://raw.githubusercontent.com/MRuihoo/rainytiger-blog/master/.zshrc
+
+cp .vimrc ~/.spf13-vim-3/.vimrc
+cp .tmux.conf ~/.tmux/.tmux.conf
+cp .zshrc ~/.zshrc
+
